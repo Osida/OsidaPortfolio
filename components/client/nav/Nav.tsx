e1b724500f6a9b07690b3d7570ad8c7740bf0eca
Osida Richards <osida@Osidas-MacBook-Air.local>
2023-08-23 07:16:18 -0400

    ui: added descriptive text to the Nav icons for better navigation experience

--- components/client/nav/Nav.tsx
+++ components/client/nav/Nav.tsx
@@ -13,21 +13,24 @@ const Nav = () => {
             initial={navMotionStyles.initial}
             animate={navMotionStyles.animate}
             transition={transition}
-            className={`backdrop-blur-sm bg-secondary/30 z-50 fixed bottom-0 left-0 right-0 py-2 lg:max-w-3xl lg:mx-auto lg:rounded-full lg:shadow-lg lg:bottom-4`}
+            className={`backdrop-blur-sm bg-secondary/30 z-50 fixed bottom-0 left-0 right-0 py-3 lg:max-w-3xl lg:mx-auto lg:rounded-full lg:shadow-lg lg:bottom-4`}
             data-cy="nav-container"
         >
-            <ul className="w-full h-full flex items-center justify-evenly" data-cy="nav-list">
+            <ul className="bg-red-2000 w-full h-full flex items-center justify-between px-2 sm:px-4 md:px-0 md:justify-evenly"
+                data-cy="nav-list">
                 {navLinks.map(({path, name, Icon}: NavLinkSchema, _) => (
                     <Link key={name} href={path} data-cy={`nav-link-container-${name}`}>
                         <motion.li
                             variants={navLinkVariants}
                             whileTap="tap"
-                            className="btn btn-ghost"
+                            className="cursor-pointer"
                             data-cy={`nav-link-${name}`}
                         >
-                            <span className="flex flex-col items-center" data-cy={`nav-link-content-${name}`}>
+                            <span className="bg-green-5000  flex flex-col items-center"
+                                  data-cy={`nav-link-content-${name}`}>
                                 <span data-cy={`nav-link-icon-${name}`}>{Icon}</span>
-                                {/*<p className="text-[8px]" data-cy={`nav-link-text-${name}`}>{name}</p>*/}
+                                <p className=" mt-1 -bottom-4 text-[0.6rem] lg:-bottom-5 lg:text-xs"
+                                   data-cy={`nav-link-text-${name}`}>{name}</p>
                             </span>
                         </motion.li>
                     </Link>
